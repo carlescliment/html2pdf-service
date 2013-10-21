e27466e5783a3df2ac8061f8b4d07c456bba1dff
Carles Climent <carlescliment@hotmail.com>
2013-10-21 10:50:41 +0200

    Fixed problem with app initialization

--- web/app.php
+++ web/app.php
@@ -5,5 +5,5 @@ use carlescliment\Html2Pdf\Application\Application;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = new Application(__DIR_ . '/../', true);
+$app = new Application(__DIR__ . '/../', true);
 $app->run();
